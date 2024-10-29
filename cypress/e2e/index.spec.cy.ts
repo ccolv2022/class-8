547328b7d7157dd0b462532b9a2658b202a5ac84
Andrew Dykstra <dykstra.a@northeastern.edu>
2024-10-29 10:31:48 -0400

    10 tests passing

--- cypress/e2e/index.spec.cy.ts
+++ cypress/e2e/index.spec.cy.ts
@@ -10,3 +10,178 @@ describe('Calculator Tests', () => {
   });
 
 });
+
+// ADDITION
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 2 when "1 + 1 = " is clicked', () => {
+
+    cy.get('.key-1').click();
+    cy.get('.key-add').click();
+    cy.get('.key-1').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '2');
+
+  });
+
+});
+
+// SUBTRACTION
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 2 when "4 - 2 = " is clicked', () => {
+
+    cy.get('.key-4').click();
+    cy.get('.key-subtract').click();
+    cy.get('.key-2').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '2');
+
+  });
+
+});
+
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 4 when "4 - 0 = " is clicked', () => {
+
+    cy.get('.key-4').click();
+    cy.get('.key-subtract').click();
+    cy.get('.key-0').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '4');
+
+  });
+
+});
+
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 0 when "4 - 4 = " is clicked', () => {
+
+    cy.get('.key-4').click();
+    cy.get('.key-subtract').click();
+    cy.get('.key-4').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '0');
+
+  });
+
+});
+
+// MULTIPLICATION
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 4 when "2 * 2 = " is clicked', () => {
+
+    cy.get('.key-2').click();
+    cy.get('.key-multiply').click();
+    cy.get('.key-2').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '4');
+
+  });
+
+});
+
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 0 when "2 * 0 = " is clicked', () => {
+
+    cy.get('.key-2').click();
+    cy.get('.key-multiply').click();
+    cy.get('.key-0').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '0');
+
+  });
+
+});
+
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 1 when "2 * 1 = " is clicked', () => {
+
+    cy.get('.key-2').click();
+    cy.get('.key-multiply').click();
+    cy.get('.key-1').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '2');
+
+  });
+
+});
+
+// DIVISION
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 2 when "4 / 2 = " is clicked', () => {
+
+    cy.get('.key-4').click();
+    cy.get('.key-divide').click();
+    cy.get('.key-2').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '2');
+
+  });
+
+});
+
+describe('Calculator Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display 1 when "4 / 1 = " is clicked', () => {
+
+    cy.get('.key-4').click();
+    cy.get('.key-divide').click();
+    cy.get('.key-1').click();
+    cy.get('.key-equals').click();
+
+    cy.get('.calculator-display').should('have.text', '4');
+
+  });
+
+});
